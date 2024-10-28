258b7c1ac885dcd79a44701bd165326e5d1f2193
HaJin0322 <92991456+hajin0322@users.noreply.github.com>
2024-10-28 13:03:03 +0900

    10/28 13:02

--- lib/screens/my_fridge.dart
+++ lib/screens/my_fridge.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:group_project/base/res/media.dart';
 import 'package:group_project/base/res/styles/app_styles.dart';
 import 'package:group_project/base/widgets/horizontal_scroll_widget_for_recently_viewed.dart';
 import 'package:group_project/base/widgets/simple_navigation_widget.dart';
 import 'package:group_project/screens/saved_recipe.dart';
 import '../base/widgets/app_bar.dart';
-import '../base/widgets/recipe_section.dart';
 
 class MyFridge extends StatelessWidget {
   const MyFridge({super.key});
