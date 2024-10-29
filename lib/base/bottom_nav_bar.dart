d271d7b2e07fed135ae9c940aa73cc4f4ade1805
Blacky197 <92620679+Blacky197@users.noreply.github.com>
2024-10-29 16:03:23 +0900

    24-10-29 16:02

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:group_project/base/res/styles/app_styles.dart';
 import 'package:group_project/screens/my_fridge.dart';
 import 'package:group_project/screens/settings.dart';
 import 'package:group_project/screens/recipe_search.dart';
@@ -13,51 +14,54 @@ class BottomNavBar extends StatefulWidget {
   State<BottomNavBar> createState() => _BottomNavBarState();
 }
 
-class _BottomNavBarState extends State<BottomNavBar> {
-  final appScreens = <Widget>[
-    const HomeScreen(),
-    const RecipeSearch(),
-    const MyFridge(),    //my_fridge 말고 변한곳 유일하게..
-    const Settings(),
-  ];
+class _BottomNavBarState extends State<BottomNavBar>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
 
-  int _selectedIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      //update ui.
-      _selectedIndex = index;
-    });
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: appScreens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          selectedItemColor: Colors.blueGrey,
-          unselectedItemColor: const Color(0xFF526400),
-          showSelectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-                label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-                label: "RecipeSearch"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_folder_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_folder_filled),
-                label: "RecipeStorage"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_settings_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_settings_filled),
-                label: "MyFridge"),
-          ]),
+      body: TabBarView(controller: _tabController, children: const [
+        HomeScreen(),
+        RecipeSearch(),
+        MyFridge(), //my_fridge 말고 변한곳 유일하게..
+        Settings(),
+      ]),
+      bottomNavigationBar: Material(
+        color: Colors.white54,
+        child: TabBar(
+          controller: _tabController,
+          labelStyle: const TextStyle(fontSize: 11, fontWeight: FontWeight.w600) ,
+          labelColor: Colors.red,
+
+          tabs: const [
+            Tab(
+              text: 'Home Screen',
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            ),
+            Tab(
+                text: 'Recipe Search',
+                icon: Icon(FluentSystemIcons.ic_fluent_search_regular)),
+            Tab(
+                text: 'My fridge',
+                icon: Icon(FluentSystemIcons.ic_fluent_folder_regular)),
+            Tab(
+                text: 'Settings',
+                icon: Icon(FluentSystemIcons.ic_fluent_settings_regular)),
+          ],
+        ),
+      ),
     );
   }
 }
