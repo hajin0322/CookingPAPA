e9715e2fc577c14b9d681d5184040ff1c5376c67
Blacky197 <92620679+Blacky197@users.noreply.github.com>
2024-11-24 21:02:43 +0900

    Blacky

--- lib/View/_RecipeSearch/search_categories.dart
+++ lib/View/_RecipeSearch/search_categories.dart
@@ -1,70 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:group_project/Repository/ingredient_list_repository.dart';
+import 'package:provider/provider.dart';
 import 'package:group_project/View/ViewAsset/styles/app_styles.dart';
 import 'package:group_project/ViewModel/ingredient_list_view_model.dart';
-import 'package:provider/provider.dart';
 
 class SearchCategories extends StatelessWidget {
-  final IngredientRepository ingredientRepository;
-  final List<String> categories;
-
-  const SearchCategories(
-      {super.key,
-      required this.ingredientRepository,
-      required this.categories});
+  const SearchCategories({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final ingredientListViewModel = context.watch<IngredientListViewModel>();
+    final categories = ingredientListViewModel.categories;
 
     return ListView.builder(
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          final category = categories[index];
-          final ingredients =
-              ingredientRepository.getIngredientsByCategory(category);
-          return Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10, horizontal: 40 - 17),
-            child: ExpansionTile(
-                backgroundColor: AppStyles.layoutColor,
-                collapsedShape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12))),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12))),
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        final category = categories[index];
+        final ingredients =
+        ingredientListViewModel.getIngredientsByCategory(category);
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 23),
+          child: ExpansionTile(
+            backgroundColor: AppStyles.layoutColor,
+            collapsedShape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            title: Text(
+              category,
+              style: AppStyles.headLineStyle3,
+            ),
+            children: ingredients.map((ingredient) {
+              final isSelected = ingredientListViewModel
+                  .ingredientList.selectedIngredientList
+                  .contains(ingredient);
+
+              return ListTile(
                 title: Text(
-                  category,
-                  style: AppStyles.headLineStyle3,
+                  ingredient.name,
+                  style: AppStyles.textStyle,
                 ),
-                children: ingredients.map((ingredient) {
-                  final isSelected = ingredientListViewModel
-                      .ingredientList.selectedIngredientList
-                      .contains(ingredient);
+                trailing: Icon(
+                  isSelected
+                      ? Icons.check_box
+                      : Icons.check_box_outline_blank,
+                  color: isSelected ? Colors.green : Colors.grey,
+                ),
+                onTap: () {
+                  final success =
+                  ingredientListViewModel.selectionChange(ingredient);
 
-                  return ListTile(
-                    title: Text(
-                      ingredient.name,
-                      style: AppStyles.textStyle,
-                    ),
-                    trailing: Icon(
-                      isSelected
-                          ? Icons.check_box
-                          : Icons.check_box_outline_blank,
-                      color: isSelected ? Colors.green : Colors.grey,
-                    ),
-                    onTap: () {
-                      final success =
-                          ingredientListViewModel.selectionChange(ingredient);
-
-                      if (!success) {
-                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                            content: Text(
-                                "You can't select more than 5 ingredients!")));
-                      }
-                    },
-                  );
-                }).toList()),
-          );
-        });
+                  if (!success) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content:
+                        Text("You can't select more than 5 ingredients!"),
+                      ),
+                    );
+                  }
+                },
+              );
+            }).toList(),
+          ),
+        );
+      },
+    );
   }
 }
