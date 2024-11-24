179c636c7b5a7212e94754018dc66cbb7c757848
Blacky197 <92620679+Blacky197@users.noreply.github.com>
2024-11-24 20:59:45 +0900

    Blacky

--- lib/View/_DetailRecipe/recipe_section.dart
+++ lib/View/_DetailRecipe/recipe_section.dart
@@ -1,129 +1,106 @@
 import 'package:flutter/material.dart';
-import '../ViewAsset/styles/app_styles.dart';
-
-class TitlePadding extends StatelessWidget {
-  final String title;
-  final TextStyle? textStyle;
-
-  const TitlePadding({
-    super.key,
-    required this.title,
-    this.textStyle,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 40, top: 10, bottom: 10),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width-120, // 80(right) + 40(left)
-        child: Text(
-          title,
-          style: textStyle ?? AppStyles.headLineStyle2,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          softWrap: true,
-        ),
-      ),
-    );
-  }
-}
-
-class ImagePadding extends StatelessWidget {
-  final String imagePath;
-  final VoidCallback onTap;
-  final double imageWidth;
-
-  const ImagePadding(
-      {super.key,
-      required this.imagePath,
-      required this.onTap,
-      this.imageWidth = 400});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 40, right: 40, top: 10, bottom: 10),
-      child: GestureDetector(
-        onTap: onTap,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.asset(
-            imagePath,
-            width: imageWidth,
-            height: 250,
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-    );
-  }
-}
+import '../../Model/recipe.dart';
 
 class RecipeSection extends StatelessWidget {
-  final String title;
-  final String imagePath;
+  final Recipe recipe;
   final VoidCallback onTap;
-  final String ico;
-  final String? additionalInfo; // 추가 정보
 
   const RecipeSection({
-    super.key,
-    required this.title,
-    required this.imagePath,
+    Key? key,
+    required this.recipe,
     required this.onTap,
-    required this.ico,
-    this.additionalInfo, // 추가 정보 입력 가능
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final aspectRatio = MediaQuery.of(context).size.aspectRatio;
-    final isTablet = MediaQuery.of(context).size.shortestSide >= 600 &&
-        aspectRatio < 1.6; // 테블릿 기준 너비 설정
-    print(isTablet);
+    final bool isTablet = _isTablet(context);
 
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: const EdgeInsets.all(16),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: isTablet ? _buildTabletLayout() : _buildMobileLayout(),
+        ),
+      ),
+    );
+  }
+
+  /// 태블릿 여부를 판단하는 메서드
+  bool _isTablet(BuildContext context) {
+    final aspectRatio = MediaQuery.of(context).size.aspectRatio;
+    return MediaQuery.of(context).size.shortestSide >= 600 && aspectRatio < 1.6;
+  }
+
+  /// 태블릿 레이아웃
+  Widget _buildTabletLayout() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildImage(flex: 1, height: 150),
+        const SizedBox(width: 16),
+        Expanded(flex: 2, child: _buildTextContent()),
+      ],
+    );
+  }
+
+  /// 모바일 레이아웃
+  Widget _buildMobileLayout() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (title.isNotEmpty)
-          Row(
-            children: [
-              TitlePadding(title: title),
-              Expanded(child: Container()),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 40),
-                child: Image.asset(
-                  ico,
-                  width: 28,
-                  height: 28,
-                ),
-              ),
-            ],
+        _buildImage(flex: null, height: 200),
+        const SizedBox(height: 16),
+        _buildTextContent(),
+      ],
+    );
+  }
+
+  /// 이미지 위젯
+  Widget _buildImage({int? flex, required double height}) {
+    final imageWidget = ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Image(
+        image: recipe.recipeImage, // ImageProvider 사용
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: height,
+        errorBuilder: (context, error, stackTrace) {
+          print("Error loading image: $error");
+          return const Placeholder(fallbackHeight: 150);
+        },
+      ),
+    );
+
+    return flex != null
+        ? Expanded(flex: flex, child: imageWidget)
+        : imageWidget;
+  }
+
+
+  /// 텍스트 콘텐츠 위젯
+  Widget _buildTextContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          recipe.recipeTitle,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 이미지
-            Expanded(
-              flex: isTablet ? 2 : 1, // 테블릿에서 이미지를 더 적게 차지
-              child: ImagePadding(
-                imagePath: imagePath,
-                onTap: onTap,
-              ),
-            ),
-            if (isTablet && additionalInfo != null) // 테블릿에서만 추가 텍스트 표시
-              Expanded(
-                flex: 1, // 나머지 공간 활용
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    additionalInfo!,
-                    style: AppStyles.headLineStyle3,
-                  ),
-                ),
-              ),
-          ],
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          recipe.recipeContent.split('\n').first,
+          style: const TextStyle(
+            fontSize: 14,
+          ),
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
         ),
       ],
     );
