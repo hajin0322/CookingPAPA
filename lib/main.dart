353f2ef4fde061e4effef3b1cc258a506c5416a7
Blacky197 <92620679+Blacky197@users.noreply.github.com>
2024-11-24 21:03:37 +0900

    Blacky

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
-import 'package:flutter/material.dart'; //
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 import 'package:group_project/View/ViewBase/bottom_nav_bar.dart';
+import 'package:group_project/ViewModel/recipe_view_model.dart';
+import 'dart:io';
+
+void debugEnvPath() {
+  final currentDir = Directory.current.path;
+  print('Current Directory: $currentDir');
+  print('Files in directory: ${Directory(currentDir).listSync()}');
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
-    await dotenv.load(fileName: "assets/.env"); // .env를 assets 디렉토리에 배치
+    await dotenv.load(fileName: "assets/.env"); // .env 파일 로드
   } catch (e) {
     print("Failed to load .env file: $e");
   }
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RecipeListViewModel()), // RecipeListViewModel 추가
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,7 +35,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNavBar(),
+      home: BottomNavBar(), // 기존 구조 유지
     );
   }
 }
