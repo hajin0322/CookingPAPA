ee9efcb1a6e212c71fde3f1c8e6d63979aa45252
Blacky197 <92620679+Blacky197@users.noreply.github.com>
2024-11-24 21:01:41 +0900

    Blacky

--- lib/View/_RecipeSearch/recipe_search.dart
+++ lib/View/_RecipeSearch/recipe_search.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:group_project/Repository/ingredient_list_repository.dart';
+import 'package:provider/provider.dart';
+import 'package:group_project/View/ViewAsset/styles/app_styles.dart';
 import 'package:group_project/View/_RecipeSearch/search_button.dart';
 import 'package:group_project/View/_RecipeSearch/search_categories.dart';
 import 'package:group_project/ViewModel/ingredient_list_view_model.dart';
-import 'package:provider/provider.dart';
-import '../../Model/ingredient_list.dart';
-import '../ViewAsset/styles/app_styles.dart';
+import 'package:group_project/Model/ingredient_list.dart';
 import '../ViewBase/app_bar.dart';
 
 class RecipeSearch extends StatefulWidget {
-  const RecipeSearch({super.key});
+  const RecipeSearch({Key? key}) : super(key: key);
 
   @override
   State<RecipeSearch> createState() => _RecipeSearchState();
 }
 
 class _RecipeSearchState extends State<RecipeSearch> {
-  late IngredientRepository _ingredientRepository;
   late List<String> categories;
 
   @override
   void initState() {
     super.initState();
-    _ingredientRepository = IngredientRepository();
-    categories = _ingredientRepository.getAllCategories();
+    categories = IngredientList().categories; // IngredientList에서 카테고리 가져오기
   }
 
   @override
@@ -34,9 +31,7 @@ class _RecipeSearchState extends State<RecipeSearch> {
         appBar: const CustomAppBar(title: "Recipe Search"),
         body: Container(
           color: AppStyles.bgColor,
-          child: SearchCategories(
-              categories: categories,
-              ingredientRepository: _ingredientRepository),
+          child: const SearchCategories(),
         ),
         floatingActionButton: const SearchButton(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
