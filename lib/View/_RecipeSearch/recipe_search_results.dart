5a8cee8eabde476b7a4fb2893c71a3a3553c8b4d
Blacky197 <92620679+Blacky197@users.noreply.github.com>
2024-11-24 21:02:11 +0900

    Blacky

--- lib/View/_RecipeSearch/recipe_search_results.dart
+++ lib/View/_RecipeSearch/recipe_search_results.dart
@@ -1,52 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:group_project/View/_DetailRecipe/recipe_section.dart';
-import '../ViewAsset/media.dart';
-import '../ViewAsset/styles/app_styles.dart';
-import '../ViewBase/app_bar.dart';
+import '../../Model/recipe.dart';
 import '../_DetailRecipe/detail_recipe.dart';
+import '../_DetailRecipe/recipe_section.dart';
 
 class RecipeSearchResults extends StatelessWidget {
-  final List<Map<String, String>> results;
+  final List<Recipe> results;
 
-  const RecipeSearchResults({super.key, required this.results});
+  const RecipeSearchResults({Key? key, required this.results}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(title: "Search Results", showBackButton: true),
+      appBar: AppBar(title: const Text("Search Results")),
       body: ListView.builder(
         itemCount: results.length,
         itemBuilder: (context, index) {
           final recipe = results[index];
-          final dishName = recipe['dishName'] ?? 'Unknown Dish';
-          final description = recipe['description'] ?? 'No description available';
-          final fullRecipe = recipe['recipe'] ?? 'No recipe provided';
-          final imagePrompt = recipe['imagePrompt'] ?? 'No image prompt available';
-
           return RecipeSection(
-            title: dishName, // 요리 이름
-            imagePath: index == 0
-                ? AppMedia.recipe1
-                : (index == 1 ? AppMedia.recipe2 : AppMedia.recipe1),
+            recipe: recipe,
             onTap: () {
-              // 클릭 시 DetailRecipe로 데이터 전달
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => DetailRecipe(
-                    dishName: dishName,
-                    recipe: fullRecipe,
-                    imagePrompt: imagePrompt,
-                  ),
+                  builder: (context) => DetailRecipe(recipe: recipe),
                 ),
               );
             },
-            additionalInfo: description, // 간단한 요리 설명
-            ico: AppMedia.iconList[index % AppMedia.iconList.length],
           );
         },
       ),
-      backgroundColor: AppStyles.bgColor,
     );
   }
 }
