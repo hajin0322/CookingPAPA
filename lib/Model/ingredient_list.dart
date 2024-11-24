e63bb03fbe9b125d6f95a626693cdeebdea8e74f
Blacky197 <92620679+Blacky197@users.noreply.github.com>
2024-11-24 20:57:49 +0900

    Blacky

--- lib/Model/ingredient_list.dart
+++ lib/Model/ingredient_list.dart
@@ -2,8 +2,20 @@ import 'package:group_project/Repository/ingredient_list_repository.dart';
 import 'package:group_project/Model/ingredient.dart';
 
 class IngredientList {
-  //repository에서 모든 리스트 받아와서 저장하기
-  final List<Ingredient> ingredientList = IngredientRepository().getAllIngredients(); //전체 재료 목록
-  Set<Ingredient> usedIngredientList = {}; // 사용된 재료 목록
-  Set<Ingredient> selectedIngredientList = {}; // 선택된 재료 목록 => api call할때 넘겨줄 자료
+  final IngredientRepository _repository = IngredientRepository();
+
+  // 전체 재료 목록
+  List<Ingredient> get ingredientList => _repository.getAllIngredients();
+
+  // 사용된 재료 목록
+  Set<Ingredient> usedIngredientList = {};
+
+  // 선택된 재료 목록
+  Set<Ingredient> selectedIngredientList = {};
+
+  // 모든 카테고리 가져오기
+  List<String> get categories {
+    return ingredientList.map((ingredient) => ingredient.category).toSet().toList();
+  }
+
 }
